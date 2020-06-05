fe8d6d47514071eea5b84174b6fee578c9ee942e
Luis Flores <lflores02142@gmail.com>
2020-06-04 19:34:41 -0700

    done with strings section

---
+++ strings.py
@@ -0,0 +1,46 @@
+# print(type("This is a string"))
+
+# username = "superCoder"
+# password = "supersecret"
+
+# # long_string = '''
+# # ^^^
+# # o o
+# # ---
+# # vvv
+# # '''
+# # print(long_string)
+
+# first_name = "Luis"
+# last_name = "Flores"
+
+# full_name = first_name + " " + last_name
+
+# print(full_name)
+
+# # string concatenation 
+# print('Hello ' + 'Luis')
+
+# print(type(str(100)))
+# Escape Sequence
+
+# weather = " \t Its \"Kind of\" sunny \n hope you have a good day"
+# print(weather) 
+
+# formatted strings
+
+# name = input("enter your name: ")
+# age = input("enter your age: ")
+
+# # print('hi {}. you are {} years old'.format(name, age)) # Python 2
+
+# print(f'Hello {name}, you are {age} years old') # Python 3
+
+string = "01234567"
+# for i in range(len(string)):
+#     print(string[i])
+# slice 
+# [start:stop:stepover]
+print(string[-1:0:-1])
+
+
